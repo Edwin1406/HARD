94ce9fdac83a3da114ceaf714e8cb1c22232035f
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-09-16 11:11:52 -0500

    Update header.php

--- views/templates/header.php
+++ views/templates/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DESARROLO</title>
+    <title>DESARROLO-NUEVO</title>
         <link rel="icon" type="image/x-icon" href="/src/img/bg_1.png">
 
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
