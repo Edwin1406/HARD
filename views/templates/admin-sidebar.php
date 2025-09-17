eb4666d951fa6b6f3c2160f995ad02e154a66d8f
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-09-17 09:00:59 -0500

    Update admin-sidebar.php

--- views/templates/admin-sidebar.php
+++ views/templates/admin-sidebar.php
@@ -16,11 +16,6 @@
 
 
 
-<script src="/assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
-<script src="/assets/js/bootstrap.bundle.min.js"></script>
-<script src="/assets/vendors/simple-datatables/simple-datatables.js"></script>
-<!-- SweetAlert2 CDN -->
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 <!-- <script src="/assets/vendors/apexcharts/apexcharts.js"></script>
 <script src="/assets/js/pages/dashboard.js"></script> -->
