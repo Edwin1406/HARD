b87268781f3b93f4a3c6731a98204018260ca15b
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-09-17 08:38:45 -0500

    Update index.php

---
+++ views/admin/dashboard/index.php
@@ -0,0 +1,28 @@
+<!-- 
+            <header class="mb-3">
+                <a href="#" class="burger-btn d-block d-xl-none">
+                    <i class="bi bi-justify fs-3"></i>
+                </a>
+            </header> -->
+<?php if ($email === 'control@megaecuador.com' || $email === 'produccion@megaecuador.com' || $email === 'pruebas@megaecuador.com') { ?>
+    <div class="page-heading">
+        <h3>GRAFICAS CONSUMO GENERAL DESPERDICIO</h3>
+
+        <!-- CERRAR SESSION  -->
+
+        <div class="d-flex justify-content-end mb-3">
+            <button class="btn btn-danger" onclick="location.href='/cerrarSesion'">Cerrar Sesión</button>
+            <br>
+            <!-- <p class="text-subtitle text-muted"><?php echo $email; ?></p> -->
+        </div>
+    </div>
+
+  
+
+<?php } else { ?>
+    <div class="page-heading">
+        <h3>Bienvenido <?php echo $nombre; ?></h3>
+        <p class="text-subtitle text-muted"><?php echo $email; ?></p>
+    </div>
+
+<?php } ?>
