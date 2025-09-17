84bb54570642a6f5a59b5ffab2ec126869ad082e
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-09-17 09:02:34 -0500

    Update admin-sidebar.php

--- views/templates/admin-sidebar.php
+++ views/templates/admin-sidebar.php
@@ -16,9 +16,13 @@
 
 
 
+<!-- <script src="/assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script> -->
+<!-- <script src="/assets/js/bootstrap.bundle.min.js"></script> -->
+<!-- <script src="/assets/vendors/simple-datatables/simple-datatables.js"></script> -->
+<!-- SweetAlert2 CDN -->
+<!-- <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> -->
 
-<!-- <script src="/assets/vendors/apexcharts/apexcharts.js"></script>
-<script src="/assets/js/pages/dashboard.js"></script> -->
+
 <?php
 $currentPath = $_SERVER['REQUEST_URI'];
 if (strpos($currentPath, '/admin/index') !== false || strpos($currentPath, '/admin/graficas/graficasConsumoGeneral') !== false || strpos($currentPath, '/admin/graficas/graficasDoblado') !== false) {
