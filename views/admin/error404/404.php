a42aa409405e0db92033ce4d0e86b297089faeb9
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-09-18 11:30:32 -0500

    Update 404.php

--- views/admin/error404/404.php
+++ views/admin/error404/404.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>404</title>
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
-    <link rel="shortcut icon" href="/src/img/logo2.png" type="image/x-icon">
+    <link rel="shortcut icon" href="/src/img/bg_1.png" type="image/x-icon">
     <link rel="stylesheet" href="/assets/css/bootstrap.css">
     <link rel="stylesheet" href="/assets/vendors/bootstrap-icons/bootstrap-icons.css">
 </head>
