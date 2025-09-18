35449542f8b82c8f4cb230e78a7dc77e99bd143d
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-09-18 11:12:41 -0500

    Update BodegaController.php

--- controllers/BodegaController.php
+++ controllers/BodegaController.php
@@ -11,11 +11,21 @@ class BodegaController
 
         $alertas = [];
 
+         session_start();
+        if (!isset($_SESSION['email'])) {
+            header('Location: /');
+        }
+
+        $nombre = $_SESSION['nombre'];
+        $email = $_SESSION['email'];
+
 
           // Render a la vista
         $router->render('admin/bodega/crearBodega', [
             'titulo' => 'Crea una Bodega', 
-            'alertas' => $alertas
+            'alertas' => $alertas,
+            'nombre' => $nombre,
+            'email' => $email
         ]);
 
 
