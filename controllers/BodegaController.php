5f40b38fbce1aec500394e8a732182379489b9db
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-09-18 16:48:05 -0500

    Update BodegaController.php

--- controllers/BodegaController.php
+++ controllers/BodegaController.php
@@ -20,16 +20,29 @@ class BodegaController
         $nombre = $_SESSION['nombre'];
         $email = $_SESSION['email'];
 
+        $bodega = new Bodega($_POST);
 
-        $bodega =  Bodega::all();
+        // $bodega =  Bodega::all();
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            $bodega->sincronizar($_POST);
+            $alertas = $bodega->validar();
+
+            if (empty($alertas)) {
+                // Guardar el registro
+                $resultado = $bodega->guardar();
+
+                if ($resultado) {
+                    header('Location: /admin/tablaBodega');
+                }
+            }
 
           // Render a la vista
         $router->render('admin/bodega/crearBodega', [
             'titulo' => 'Crea una Bodega', 
             'alertas' => $alertas,
             'nombre' => $nombre,
-            'email' => $email,
-            'bodega' => $bodega
+            'email' => $email
         ]);
 
 
