89201d1fa0b785c4a8db6c913ce6e1349de6f270
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-09-18 21:05:28 -0500

    .

--- views/admin/marca/crearMarca.php
+++ views/admin/marca/crearMarca.php
@@ -78,10 +78,6 @@
                                         </div>
                                     </div>                                  
 
-
-                                 
-
-
                                     <div class="col-12 d-flex justify-content-end">
                                         <button type="submit" id="btnRegistrar" class="btn btn-primary me-1 mb-1">Registrar</button>
                                         <button type="reset" class="btn btn-light-secondary me-1 mb-1">Limpiar</button>
