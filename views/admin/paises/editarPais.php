d90cea6e0e71207c760b566bdecbcd2317168287
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-09-19 12:53:16 -0500

    Update editarPais.php

--- views/admin/paises/editarPais.php
+++ views/admin/paises/editarPais.php
@@ -69,7 +69,7 @@
                     </div>
                     <div class="card-content">
                         <div class="card-body">
-                            <form class="form" method="POST" action="/admin/paises/crearPais" onsubmit="return bloquearBoton(this)">
+                            <form class="form" method="POST" action="/admin/paises/crearPais?id=<?php echo s($pais->id); ?>" onsubmit="return bloquearBoton(this)">
                                 <div class="row">
 
 
