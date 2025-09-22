1af93cc3e0e003836227c8482553d576ea39467b
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-09-21 19:42:04 -0500

    Update crearPruebas.php

--- views/admin/pruebas/crearPruebas.php
+++ views/admin/pruebas/crearPruebas.php
@@ -5,7 +5,7 @@
 </header> -->
 
 <div class="page-heading">
-    
+
 
     <div class="toast-container position-fixed top-0 end-0 p-3">
         <div id="toastExito" class="toast align-items-center text-bg-success border-0" role="alert" aria-live="assertive" aria-atomic="true">
@@ -44,7 +44,11 @@
     </section> -->
 
 
-    <!-- // Basic multiple Column Form section start -->
+    
+
+
+
+
     <section id="multiple-column-form">
         <div class="row match-height">
             <div class="col-12">
@@ -62,112 +66,7 @@
                                             <label for="nota_pedido">Nota de pedido N°</label>
                                             <input type="number" id="nota_pedido" class="form-control"
                                                 placeholder="Nota de pedido N°" name="nota_pedido" step="0.01"
-                                                value="<?= $id_nota; ?>" readonly>
-                                        </div>
-                                    </div>
-
-
-                                    <div class="col-md-3 col-12">
-                                        <div class="form-group">
-                                            <label for="num_pedido"></label>
-                                            <input type="number" id="num_pedido" class="form-control"
-                                                placeholder="# Pedido" name="num_pedido" step="0.01">
-                                        </div>
-                                    </div>
-
-                                    <div class="col-md-3 col-12">
-                                        <div class="form-group">
-                                            <label for="num_importacion"></label>
-                                            <input type="number" id="num_importacion" class="form-control"
-                                                placeholder="# Importación" name="num_importacion" step="0.01">
-                                        </div>
-                                    </div>
-
-                                    <!-- fecha -->
-                                    <div class="col-md-3 col-12">
-                                        <div class="form-group">
-                                            <label for="fecha">Fecha</label>
-                                            <input type="date" id="fecha" class="form-control"
-                                                name="fecha" value="<?php echo date('Y-m-d'); ?>" required>
-                                        </div>
-                                    </div>
-
-
-                                    <div class="row">
-
-                                        <!-- importador select -->
-                                        <div class="col-md-5 col-12">
-                                            <div class="form-group">
-                                                <label for="importador">Importador</label>
-                                                <select id="importador" class="choices form-control" name="importador">
-                                                    <option value="" disabled selected>Seleccione un importador</option>
-                                                    <option value="IMPORTADORA MEGA ECUADOR S.A">IMPORTADORA MEGA ECUADOR S.A</option>
-                                                    <option value="IMPORTADORA MEGA DEL PERU S.A.C">IMPORTADORA MEGA DEL PERU S.A.C</option>
-                                                    <option value="IMPORTADORA MEGA DE COLOMBIA S.A.S">IMPORTADORA MEGA DE COLOMBIA S.A.S</option>
-                                                    <option value="IMPORTADORA MEGA DE CHILE LTDA">IMPORTADORA MEGA DE CHILE LTDA</option>
-                                                    <option value="IMPORTADORA MEGA DE MEXICO S.A. DE C.V.">IMPORTADORA MEGA DE MEXICO S.A. DE C.V.</option>
-                                                </select>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-5 col-12">
-                                            <div class="form-group">
-                                                <label for="exportador">Exportador</label>
-                                                <select id="exportador" class="choices form-control" name="exportador">
-                                                    <option value="" disabled selected>Seleccione un exportador</option>
-                                                    <option value="INDUSTRIAS CARTONAJES S.A">INDUSTRIAS CARTONAJES S.A</option>
-                                                    <option value="INDUSTRIAS CARTONAJES DEL PERU S.A.C">INDUSTRIAS CARTONAJES DEL PERU S.A.C</option>
-                                                    <option value="INDUSTRIAS CARTONAJES DE COLOMBIA S.A.S">INDUSTRIAS CARTONAJES DE COLOMBIA S.A.S</option>
-                                                    <option value="INDUSTRIAS CARTONAJES DE CHILE LTDA">INDUSTRIAS CARTONAJES DE CHILE LTDA</option>
-                                                    <option value="INDUSTRIAS CARTONAJES DE MEXICO S.A. DE C.V.">INDUSTRIAS CARTONAJES DE MEXICO S.A. DE C.V.</option>
-                                                    <option value="INDUSTRIAS CARTONAJES DE ECUADOR S.A.">INDUSTRIAS CARTONAJES DE ECUADOR S.A.</option>
-                                                </select>
-                                            </div>
-                                        </div>
-
-
-                                        <style>
-
-
-
-
-                                        </style>
-
-
-
-
-
-
-                                        <div class="col-12 d-flex justify-content-end">
-                                            <button type="submit" class="btn btn-primary me-1 mb-1">Agregar</button>
-                                            <button type="reset" class="btn btn-light-secondary me-1 mb-1">Limpiar</button>
-                                        </div>
-                                    </div>
-                            </form>
-                        </div>
-                    </div>
-
-
-                </div>
-            </div>
-        </div>
-    </section>
-    <section id="multiple-column-form">
-        <div class="row match-height">
-            <div class="col-12">
-                <div class="card">
-
-                    <?php include_once __DIR__ . '/../../templates/alertas.php'  ?>
-                    <div class="card-content">
-                        <div class="card-body">
-                            <form class="form" method="POST" action="/admin/pruebas/crearPruebas" enctype="multipart/form-data" onsubmit="return bloquearBoton(this)">
-                                <div class="row">
-
-
-                                    <div class="col-md-3 col-12">
-                                        <div class="form-group">
-                                            <label for="nota_pedido">Nota de pedido N°</label>
-                                            <input type="number" id="nota_pedido" class="form-control"
-                                                placeholder="Nota de pedido N°" name="nota_pedido" step="0.01">
+                                                value="<?php echo $id_nota; ?>" readonly>
                                         </div>
                                     </div>
 
