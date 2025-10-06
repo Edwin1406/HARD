3004fc07ec9394193919f45b8c31f39547e4069c
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-10-05 20:19:09 -0500

    Update Carrito2.php

--- models/Carrito2.php
+++ models/Carrito2.php
@@ -8,21 +8,26 @@ class Carrito2 extends ActiveRecord {
         'id',
         'Codigo_Nota_Pedido',
         'prenda',
-        'cantidad'
+        'cantidad',
+        'precio_unitario',
+        'total'
     ];
 
     public $id;
     public $Codigo_Nota_Pedido;
     public $prenda;
     public $cantidad;
-    
-    
+    public $precio_unitario;
+    public $total;
+
     public function __construct($args = [])
     {
         $this->id = $args['id'] ?? null;
         $this->Codigo_Nota_Pedido = $args['Codigo_Nota_Pedido'] ?? null;
         $this->prenda = $args['prenda'] ?? null;
         $this->cantidad = $args['cantidad'] ?? 0;
+        $this->precio_unitario = $args['precio_unitario'] ?? 0.0;
+        $this->total = $args['total'] ?? 0.0;
 
     }
 }
