77774f2789219ea78c1d0f673a7b45490a789bbd
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-10-05 22:16:04 -0500

    Update crearPruebas.php

--- views/admin/pruebas/crearPruebas.php
+++ views/admin/pruebas/crearPruebas.php
@@ -292,8 +292,8 @@ $selIf    = function ($left, $right) {
 
 
 <!-- Bootstrap CSS (ACTIVADO) -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
+<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"> -->
+<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet"> -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 <!-- ====== Handsontable ====== -->
