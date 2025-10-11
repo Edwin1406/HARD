799632c95a7aca7866769cc7dc36fdb377d4d512
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-10-11 06:34:11 -0500

    Update crearPruebas.php

--- views/admin/pruebas/crearPruebas.php
+++ views/admin/pruebas/crearPruebas.php
@@ -478,7 +478,7 @@ $selIf    = function ($left, $right) {
     }
 
     #hot-min {
-        height: clamp(160px, 70vh, 740px)
+        height: clamp(160px, 60vh, 99940px)
     }
 
     .handsontable th,
