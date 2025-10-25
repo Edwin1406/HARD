4e1f6b1b164efa92255e6420bf812d9476b0760f
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-10-24 20:30:43 -0500

    Update PruebasController.php

--- controllers/PruebasController.php
+++ controllers/PruebasController.php
@@ -137,7 +137,7 @@ class PruebasController
         $marca   = Marca::all();
         $prendas = Prenda::all();
 
-        // debuguear($prendas);
+        debuguear($paises);
 
         // Info de la nota (como lo tenías)
         $informacionNota = NotaPedido::where('Codigo_Nota_Pedido', $id_nota);
