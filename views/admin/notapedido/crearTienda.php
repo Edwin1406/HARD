516027471c841fde15a244697d21f749cc5c137d
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-10-26 20:43:53 -0500

    Update crearTienda.php

--- views/admin/notapedido/crearTienda.php
+++ views/admin/notapedido/crearTienda.php
@@ -66,7 +66,7 @@
 
 
 
-                            <form class="form" method="POST" action="/admin/notaPedido/crearNota" onsubmit="return bloquearBoton(this)">
+                            <form class="form" method="POST" action="/admin/notaPedido/crearTienda" onsubmit="return bloquearBoton(this)">
                                 <div class="row">
 
                                     <div class="col-md-3 col-12">
