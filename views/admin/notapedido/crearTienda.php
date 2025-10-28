7e8d60102f12aad5f007d82ad4beb47cf17dd952
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-10-28 14:42:04 -0500

    Update crearTienda.php

--- views/admin/notapedido/crearTienda.php
+++ views/admin/notapedido/crearTienda.php
@@ -255,7 +255,7 @@
 <section class="section">
     <div class="card">
         <div class="card-header">
-            Tabla de registros de Bodegas
+            Tabla de registros de Tiendas asociadas a la Nota de Pedido
         </div>
         <div class="card-body">
             <table class="table table-striped" id="table1">
