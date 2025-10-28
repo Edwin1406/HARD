c531db7198bdb5b0884ac07ca75dbe13f2ddc8cc
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-10-28 12:35:08 -0500

    Update crearNota.php

--- views/admin/notapedido/crearNota.php
+++ views/admin/notapedido/crearNota.php
@@ -73,7 +73,8 @@
                                         <div class="form-group">
                                             <label for="Codigo_Nota_Pedido">Nota de pedido N°</label>
                                             <input type="number" id="Codigo_Nota_Pedido" class="form-control"
-                                                placeholder="Nota de pedido N°" name="Codigo_Nota_Pedido">
+                                                placeholder="Nota de pedido N°" name="Codigo_Nota_Pedido"
+                                                value="<?php echo s($notaPedido->Codigo_Nota_Pedido+1); ?>">
                                         </div>
                                     </div>
 
