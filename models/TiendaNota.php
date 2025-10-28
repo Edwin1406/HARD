e6899a2ce086eb2923e506b14016e1303099a54d
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-10-28 13:43:33 -0500

    Update TiendaNota.php

--- models/TiendaNota.php
+++ models/TiendaNota.php
@@ -7,12 +7,21 @@ class TiendaNota extends ActiveRecord {
     protected static $columnasDB = [
         'id',
         'Codigo_Nota_Pedido',
-        'tienda'
+        'tienda',
+        'ciudad',
+        'pais',
+        'fecha',
+        'num_factura'
     ];
 
     public $id;
     public $Codigo_Nota_Pedido;
     public $tienda;
+    public $ciudad;
+    public $pais;
+    public $fecha;
+    public $num_factura;
+    
 
 
     public function __construct($args = [])
@@ -20,13 +29,33 @@ class TiendaNota extends ActiveRecord {
         $this->id               = $args['id'] ?? null;
         $this->Codigo_Nota_Pedido= $args['Codigo_Nota_Pedido'] ?? null;
         $this->tienda           = $args['tienda'] ?? '';
-        
+        $this->ciudad          = $args['ciudad'] ?? '';
+        $this->pais            = $args['pais'] ?? '';
+        $this->fecha          = $args['fecha'] ?? '';
+        $this->num_factura    = $args['num_factura'] ?? '';
     }
 
     public function validar() {
         if(!$this->tienda) {
             self::$alertas['error'][] = 'El Campo Tienda es Obligatorio';
+
         }
+
+        if(!$this->ciudad) {
+            self::$alertas['error'][] = 'El Campo Ciudad es Obligatorio';
+
+        }
+
+
+
+        if(!$this->fecha) {
+            self::$alertas['error'][] = 'El Campo Fecha es Obligatorio';
+        }
+        
+
+
+
+
         return self::$alertas;
     }
 }
