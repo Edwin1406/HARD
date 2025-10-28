ff6a9c85dcde9001147c20ef1acbda0082ab513c
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-10-28 13:22:37 -0500

    Update NotaPedidoController.php

--- controllers/NotaPedidoController.php
+++ controllers/NotaPedidoController.php
@@ -127,7 +127,7 @@ $ultimoCodigo = NotaPedido::ultimoCodigo();
         // Obtener id de nota pedido
         $id_nota_pedido = $_GET['id'] ?? 0;
         // sanitize
-        $id_nota_pedido = filter_var($id_nota_pedido, FILTER_SANITIZE_NUMBER_INT);
+        $id_nota_pedido = intval($id_nota_pedido, FILTER_SANITIZE_NUMBER_INT);
 
         debuguear($id_nota_pedido);
 
