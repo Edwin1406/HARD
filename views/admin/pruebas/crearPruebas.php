15ebaacc4f9ea27aeaa14d1c5b719954e71893e0
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-10-28 17:05:14 -0500

    d

--- views/admin/pruebas/crearPruebas.php
+++ views/admin/pruebas/crearPruebas.php
@@ -386,6 +386,7 @@ $selIf    = function ($left, $right) {
                             onsubmit="return bloquearBoton(this)">
 
                             <input type="hidden" name="id_nota" value="<?= htmlspecialchars($id_nota ?? '') ?>">
+                            <input type="hidden" name="id_tienda" value="<?= htmlspecialchars($_GET['id'] ?? '') ?>">
 
                             <div class="row">
 
