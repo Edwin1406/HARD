5fb1c5c18906a77475fa43c673dd49b1c0ac26d3
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-10-28 13:28:16 -0500

    Update NotaPedidoController.php

--- controllers/NotaPedidoController.php
+++ controllers/NotaPedidoController.php
@@ -141,7 +141,7 @@ $ultimoCodigo = NotaPedido::ultimoCodigo();
         $email = $_SESSION['email'];
 
 
-        $notaPedido = NotaPedido::find('1047');
+        $notaPedido = NotaPedido::where('Codigo_Nota_Pedido', '1047');
         debuguear($notaPedido);
 
         $tiendaNota = new TiendaNota;
