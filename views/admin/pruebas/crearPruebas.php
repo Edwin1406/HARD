f6961bdd801411e71b51f2a06985c1f84f7dfd46
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-10-29 17:20:39 -0500

    Update crearPruebas.php

--- views/admin/pruebas/crearPruebas.php
+++ views/admin/pruebas/crearPruebas.php
@@ -1028,7 +1028,7 @@ $selIf    = function ($left, $right) {
         height: container.clientHeight,
         theme: '',  // Tema minimalista
         rowHeights: 25,     // Filas más delgadas
-        colWidths: [30, 80, 100, 80, 100, 100, 120, 100, 100, 100, 120, 120, 100, 100, 150, 120], // Anchos de columnas ajustados
+        colWidths: [40, 40, 40, 80, 100, 100, 120, 100, 100, 100, 120, 120, 100, 100, 150, 120], // Anchos de columnas ajustados
         filters: true,
         dropdownMenu: true,
         columnSorting: true,
