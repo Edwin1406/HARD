6c5df04c27538b9dc7bd9211de27f5a689acb9be
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2025-10-29 20:27:54 -0500

    Update PruebasController.php

--- controllers/PruebasController.php
+++ controllers/PruebasController.php
@@ -206,9 +206,10 @@ class PruebasController
             // Mapear POST -> modelo (solo los campos pedidos)
             // $carrito->Codigo_Nota_Pedido = $id_nota;  
 
-            $id_tienda = $_POST['id_tienda'] ?? null;
+            $id_tienda = $_GET['id'] ?? null;
 
-            // debuguear($id_tienda);
+
+            debuguear($id_tienda);
 
 
             $id_nota = $_POST['id_nota'] ?? 0;
