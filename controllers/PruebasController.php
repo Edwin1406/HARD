a799d52a1ba9e3a2f14fc7144430d127aae0b54f
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2026-01-15 12:18:07 -0500

    Update PruebasController.php

--- controllers/PruebasController.php
+++ controllers/PruebasController.php
@@ -1071,7 +1071,8 @@ class PruebasController
         $pdf->Cell($w['total'], 7, 'TOTAL', 1, 0, 'C', true);
         $pdf->Cell($w['fact'], 7, 'FACTURA', 1, 0, 'C', true);
         $pdf->Cell($w['marca'], 7, 'MARCA', 1, 0, 'C', true);
-        $pdf->Cell($w['orig'], 7, 'ORIGEN', 1, 1, 'C', true);
+        $pdf->Cell($w['orig'], 7, 'ORIGEN', 1, 0, 'C', true);
+        $pdf->Cell($w['caja'], 7, 'CAJA', 1, 1, 'C', true);
 
         $pdf->SetFont('helvetica', '', 9);
 
@@ -1088,7 +1089,7 @@ class PruebasController
             $fact  = $it->num_factura ?? '';
             $marca = $it->marca ?? '';
             $orig  = $it->pais ?? '';
-
+            $caja  = $it->caja ?? '';
             $totalGeneral += $tot;
 
             $pdf->Cell($w['etq'], 6, (string)$etq, 1, 0, 'C');
@@ -1105,7 +1106,8 @@ class PruebasController
             $pdf->Cell($w['total'], 6, number_format($tot, 2, '.', ''), 1, 0, 'R');
             $pdf->Cell($w['fact'], 6, (string)$fact, 1, 0, 'C');
             $pdf->Cell($w['marca'], 6, (string)$marca, 1, 0, 'C');
-            $pdf->Cell($w['orig'], 6, (string)$orig, 1, 1, 'C');
+            $pdf->Cell($w['orig'], 6, (string)$orig, 1, 0, 'C');
+            $pdf->Cell($w['caja'], 6, (string)$caja, 1, 1, 'C');
         }
 
         // Total general
