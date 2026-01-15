fb9a62bac9b7f1dbd69292266bfbcd36ff74ef18
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2026-01-15 13:49:24 -0500

    Update sidebar-only.php

--- views/templates/sidebar-only.php
+++ views/templates/sidebar-only.php
@@ -163,13 +163,13 @@
               <li class="nav-item">
                 <a data-bs-toggle="collapse" href="#base">
                   <i class="fas fa-layer-group"></i>
-                  <p>Base</p>
+                  <p>crear marca</p>
                   <span class="caret"></span>
                 </a>
                 <div class="collapse" id="base">
                   <ul class="nav nav-collapse">
                     <li>
-                      <a href="components/avatars.html">
+                      <a href="/admin/marca/crearMarca">
                         <span class="sub-item">Avatars</span>
                       </a>
                     </li>
