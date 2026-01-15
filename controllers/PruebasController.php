69bfb48179423869153e681fdd036e73e7047e5c
Edwin1406 <117423831+Edwin1406@users.noreply.github.com>
2026-01-15 11:10:30 -0500

    Update PruebasController.php

--- controllers/PruebasController.php
+++ controllers/PruebasController.php
@@ -741,7 +741,7 @@ public static function crearPruebasAjax()
         $informacionNota = NotaPedido::where('Codigo_Nota_Pedido', $id_nota);
         $carritoItems = Carrito2::whereArray(['Codigo_Nota_Pedido' => $id_nota]);
 
-        debuguear($carritoItems);
+        debuguear($informacionNota);
         // debuguear($carritoItems);
 
         // Aquí iría la lógica para generar el PDF
